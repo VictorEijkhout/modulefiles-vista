da01b59ea0c8a9561c99dff695f264d5b9715b19
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-02-24 17:44:31 -0600

    set up my own cuda

---
+++ Core/cuda/12.61.lua
@@ -0,0 +1,53 @@
+local help_message = [[
+Version 12.6 by Victor
+]]
+
+help(help_message,"\n")
+
+whatis("Name: NVIDIA Compilers")
+whatis("Version: 12.6")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: https://docs.nvidia.com/hpc-sdk/compilers/hpc-compilers-user-guide/index.html")
+
+whatis("Name: cuda")
+whatis("Version: 12.6")
+whatis("Category: Compiler, Runtime Support")
+whatis("Description: NVIDIA CUDA Toolkit for Linux")
+whatis("URL: http://www.nvidia.com/cuda")
+
+help(help_message,"\n")
+
+-- Create environment variables
+local cuda_dir = "/home1/apps/nvidia/Linux_aarch64/24.9/cuda/12.6"
+local gcc_dir = "/opt/apps/gcc/14.2.0"
+
+local cuda_bin=pathJoin(cuda_dir,"bin")
+local gcc_bin=pathJoin(gcc_dir,"bin")
+
+local cuda_lib=pathJoin(cuda_dir,"lib64")
+local gc_lib=pathJoin(gcc_dir,"lib64")
+
+local cuda_inc=pathJoin(cuda_dir,"include")
+
+setenv("TACC_CUDA_DIR",cuda_dir)
+setenv("NVHPC_CUDA_HOME"       ,cuda_dir)
+setenv("TACC_CUDA_BIN",cuda_bin)
+setenv("TACC_CUDA_LIB",cuda_lib)
+setenv("TACC_CUDA_INC",cuda_inc)
+
+prepend_path("PATH"           ,gcc_bin)
+prepend_path("PATH"           ,cuda_bin)
+
+prepend_path("LD_LIBRARY_PATH",gcc_lib)
+prepend_path("LD_LIBRARY_PATH",cuda_lib)
+
+prepend_path("LIBRARY_PATH",pathJoin(cuda_lib,"stubs"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(cuda_dir,"extras/CUPTI/lib64"))
+prepend_path("INCLUDE"        ,cuda_inc)
+prepend_path("CPATH"          ,cuda_inc)
+
+-- Adding to MODULEPATH for CUDA-dependent packages
+prepend_path("MODULEPATH"     ,pathJoin("/opt/apps","nvidia24", "cuda12","modulefiles"))
+add_property("arch","gpu")
+
--- Core/nvhpc-hpcx/24.3.lua
+++ Core/nvhpc-hpcx/24.3.lua
@@ -1,9 +1,56 @@
-inherit()
+-- inherit()
 
 local compiler = "nvidia"
 local MP_ROOT  = os.getenv("VICTOR_MODULEPATH_ROOT")
 local version  = "24.3"
 
+--
+-- System module
+--
+
+local nvhome = "/opt/nvidia/hpc_sdk"
+local target = "Linux_aarch64"
+-- local version = "24.3"
+local cudaver = "trampoline"
+
+local nvcudadir = pathJoin( nvhome,target,version,"cuda" )
+local nvcompdir = pathJoin( nvhome,target,version,"compilers" )
+local nvmathdir = pathJoin( nvhome,target,version,"math_libs" )
+local nvcommdir = pathJoin( nvhome,target,version,"comm_libs" )
+
+setenv( "NVHPC", nvhome )
+setenv( "NVHPC_ROOT", pathJoin( nvhome,target,version ) )
+setenv( "TACC_CC", pathJoin( nvcompdir,"bin","nvc" ) )
+setenv( "TACC_CXX", pathJoin( nvcompdir,"bin","nvc++" ) )
+setenv( "TACC_FC", pathJoin( nvcompdir,"bin","nvfortran" ) )
+setenv( "TACC_F90", pathJoin( nvcompdir,"bin","nvfortran" ) )
+setenv( "TACC_F77", pathJoin( nvcompdir,"bin","nvfortran" ) )
+setenv( "TACC_CPP", "cpp" )
+
+prepend_path( "PATH", pathJoin( nvcudadir,"bin" ) )
+prepend_path( "PATH", pathJoin( nvcompdir,"bin" ) )
+prepend_path( "PATH", pathJoin( nvcompdir,"extras","qd","bin" ) )
+
+prepend_path( "LD_LIBRARY_PATH", pathJoin( nvcudadir,"lib64" ) )
+prepend_path( "LD_LIBRARY_PATH", pathJoin( nvcudadir,"extras","CUPTI","lib64" ) )
+prepend_path( "LD_LIBRARY_PATH", pathJoin( nvcompdir,"extras","qd","lib" ) )
+prepend_path( "LD_LIBRARY_PATH", pathJoin( nvcompdir,"lib" ) )
+prepend_path( "LD_LIBRARY_PATH", pathJoin( nvmathdir,"lib64" ) )
+prepend_path( "LD_LIBRARY_PATH", pathJoin( nvcommdir,"nccl","lib" ) )
+prepend_path( "LD_LIBRARY_PATH", pathJoin( nvcommdir,"nvshmem","lib" ) )
+
+prepend_path( "CPATH", pathJoin( nvmathdir,"include" ) )
+prepend_path( "CPATH", pathJoin( nvcommdir,"nccl","include" ) )
+prepend_path( "CPATH", pathJoin( nvcommdir,"nvshmem","include" ) )
+prepend_path( "CPATH", pathJoin( nvcompdir,"extras","qd","include","qd" ) )
+prepend_path( "PATH", pathJoin( nvcommdir,"hpcx","bin" ) )
+
+--
+-- My additions
+--
+
+setenv( "TACC_FAMILY_COMPILER", compiler )
+setenv( "TACC_FAMILY_COMPILER_VERSION", version )
 prepend_path("MODULEPATH", pathJoin(MP_ROOT, "Compiler",compiler,version))
 
 family("compiler")
